1df703b85238ea674f09e264b93e81c80e7a6794
Christine Galeener <christine@Darrens-MacBook-Air.local>
2018-01-24 01:03:00 -0800

    created game logic pt 2

--- js/main.js
+++ js/main.js
@@ -2,14 +2,25 @@ console.log("Up and running!")
 
 var cards = ['queen', 'queen', 'king', 'king'];
 cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push (cardOne);
-console.log("User flipped queen.");
-var cardTwo = cards[2];
-cardsInPlay.push (cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.")
+	console.log("Sorry, try again.");
+}
+};
+
+var flipCard = function (cardID) {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match');
+	} else {
+		alert('Sorry, try again.')
 	}
+	console.log('User flipped' + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
